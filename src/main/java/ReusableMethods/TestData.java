5ce3f1d62886530d984742900221cc1e2f80389b
momtazrasel <momtazrasel8@gmail.com>
2025-08-08 11:24:15 +0600

    login automation done

---
+++ src/main/java/ReusableMethods/TestData.java
@@ -0,0 +1,14 @@
+package ReusableMethods;
+
+import org.openqa.selenium.WebDriver;
+
+public class TestData {
+    private WebDriver driver;
+
+    public TestData(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
+}
